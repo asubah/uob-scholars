43998ffc83850cc93953944d8aaa3b1d0bdb87bd
asubah <abdulla.e.subah@gmail.com>
2024-06-11 21:24:30 +0300

    write

--- fetch.py
+++ fetch.py
@@ -1,14 +1,23 @@
 from scholarly import scholarly
+import pickle
 
 org = scholarly.search_org("University of Bahrain")
 authors = scholarly.search_author_by_organization(org[0]['id'])
 
+with open("authors.txt", "w") as f:
+    for author in authors:
+        f.write(str(author))
+        f.write("\n")
 
-for i in range(1, 6):
-    author = next(authors)
-    scholarly.pprint(author)
-    author_details = scholarly.fill(author)
-    scholarly.pprint(author_details)
-    publications = author_details["publications"][0]
-    pub_details = scholarly.fill(publications)
-    scholarly.pprint(pub_details)
+# with open('authors.pkl', 'wb') as outp:
+#     pickle.dump(authors, outp, pickle.HIGHEST_PROTOCOL)
+
+
+# for i in range(1, 6):
+#     author = next(authors)
+#     scholarly.pprint(author)
+#     author_details = scholarly.fill(author)
+#     scholarly.pprint(author_details)
+#     publications = author_details["publications"][0]
+#     pub_details = scholarly.fill(publications)
+#     scholarly.pprint(pub_details)
